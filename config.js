70a210d95f13eb4c6f3f1f438ea19958c0589ad7
JimBrennan <jbrennan95@gmail.com>
2014-11-09 20:28:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ _configOptions = {
 	fieldName_Level: "level",
 
 	//Initial zoom level for overview map
-	initialZoomLevel: 1,
+	initialZoomLevel: 4,
 	
 	//Initial overview map zoom level for wider map aspect ratios
-	initialZoomLevelWide: 1,
+	initialZoomLevelWide: 5,
 	
 	//If no zoom level is encoded for the feature, use this zoom 
 	//level for the large scale map
